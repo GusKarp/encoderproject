b988e7e7b3165ea1173cbfdb286ab467c3b5a444
Gus Karp <augustus.karp@users.noreply.github.com>
2016-10-20 09:17:10 -0700

    parts 1-4 done (with the help of teacher)

---
+++ encoder.js
@@ -0,0 +1,57 @@
+$("inputArea").keydown(function(e){
+  translation[e.keycode]
+$("textArea").append(translation[e.keycode])
+});
+
+$("input[type='radio']").click(function(){
+  $("input:checked").prop('checked', false);
+  $(this).prop('checked', true);
+});
+
+var translation = {
+64:"z",
+65:"a",
+66:"b",
+67:"c",
+68:"d",
+69:"e",
+70:"f",
+71:"g",
+72:"h",
+73:"i",
+74:"j",
+75:"k",
+76:"l",
+77:"m",
+78:"n",
+79:"o",
+80:"p",
+81:"q",
+82:"r",
+83:"s",
+84:"t",
+85:"u",
+86:"v",
+87:"w",
+88:"x",
+89:"y",
+90:"z",
+91:"a"
+};
+
+$("#inputArea").keydown(function(e) {
+
+  if ($("input:checked").val() == "echo") {
+    $("#textArea").append(translation[e.keycode]);
+  }
+
+  if($("input:checked").val() == "caesar cipher") {
+    $("#textArea").append(translation[e.keycode - 1]);
+  }
+});
+
+$("#translationInputText").keydown(function(e) {
+  if (e.keycode == 13) {
+
+  }
+});
